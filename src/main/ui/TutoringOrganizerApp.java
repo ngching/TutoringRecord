6fcb64fa0272a99d2c6b587b7f4c67b382fc209f
tng19 <tng19@student.ubc.ca>
2024-10-11 13:13:00 -0700

    Added specification to TutoringOrganizerApp in ui

---
+++ src/main/ui/TutoringOrganizerApp.java
@@ -0,0 +1,150 @@
+package ui;
+
+import java.util.Scanner;
+
+import model.Student;
+import model.TutoringTimetable;
+
+// A tutoring organizer appliation that allows users to record their students and their tutoring
+// hours with each students
+public class TutoringOrganizerApp {
+
+    private Student selectedStudent;
+    private TutoringTimetable students;
+    private Scanner input;
+
+    // EFFECTS: runs the tutoring organizer application
+    public TutoringOrganizerApp() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes user input
+    private void runOrganizer() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initializes the applcation with starting values (empty list of
+    // students)
+    private void init() {
+        // stub
+    }
+
+    // EFFECTS: displays a list of commands the user could do in the main menu
+    private void displayMainMenu() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes the command in the main menu according to the input command
+    private void processMainMenuCommand(String command) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds a new student to the list of students
+    private void addNewStudent() {
+        // stub
+    }
+
+    // EFFECTS: displays all students by their names at once
+    private void displayStudentsNames() {
+        // stub
+    }
+
+    // EFFECTS: displays all lessons for every students
+    private void displayAllLessons() {
+        // stub
+    }
+
+    // EFFECTS: displays the lessons of the given student in a readable way
+    private void displayLessons(Student student) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: displays students names & displays and handles the student list menu
+    // according to the input command
+    private void displayNamesAndHandleStudentListMenu() {
+        // stub
+    }
+
+    // EFFECTS: displays all students names
+    private void displayNames() {
+        // stub
+    }
+
+    // EFFECTS: displays a list of commands the user could do in the student list
+    // menu
+    private void displayStudentListMenu() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes the command in the student list menu according to the
+    // input command
+    private void processStudentListMenuCommand(String command) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: removes a student from the list of students
+    private void removeStudent() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: selects a student from the list of students and shows the details
+    private void selectStudent() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: shows the details of the selected student, displays and handles the
+    // student lessons menu according to the input command
+    private void showDetailsAndHandleStudentLessonsMenu() {
+        // stub
+    }
+
+    // EFFECTS: displays the details of the selected student
+    private void showSelectedStudentDetails() {
+        // stub
+    }
+
+    // EFFECTS: displays a list of commands the user could do in the student's
+    // lessons menu
+    private void displayStudentLessonsMenu() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes the command in the student's lessons menu according to the
+    // input command
+    private void processStudentLessonsMenuCommand(String command) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds a lesson with the selected student
+    private void addLessonWithStudent() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: marks the selected lesson as paid
+    private void markLessonAsPaid() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: marks the selected lesson as unpaid
+    private void markLessonAsUnpaid() {
+        // stub
+    }
+
+    // EFFECTS: prints out a divider (a line of equals signs) to make things clear
+    private void printDivider() {
+        // stub
+    }
+}
