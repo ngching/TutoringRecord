c18ed784a266605362a690b6c01e582fff85991d
tng19 <tng19@student.ubc.ca>
2024-11-22 03:54:06 -0800

    Modifiy SplashScreen

--- src/main/ui/SplashScreen.java
+++ src/main/ui/SplashScreen.java
@@ -9,26 +9,33 @@ import javax.swing.JWindow;
 import javax.swing.SwingConstants;
 
 public class SplashScreen extends Frame {
+    private JWindow window;
 
+    // EFFECTS: create a splash screen when starting the application
     public SplashScreen() {
-        JWindow window = new JWindow();
-
-        window.getContentPane().add(getJLabelImage());
-
+        window = new JWindow();
         window.setSize(400, 300);
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+        window.getContentPane().add(getJLabelImage());
+        autoClose();
+    }
+
+    // EFFECTS: return the image from folder "assets"
+    public JLabel getJLabelImage() {
+        Icon icon = new ImageIcon("assets/splash.png");
+        JLabel label = new JLabel("", icon, SwingConstants.CENTER);
+        return label;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: automatically close the screen after a few seconds
+    public void autoClose() {
         try {
             Thread.sleep(2500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        window.setVisible(false);
-    }
-
-    public JLabel getJLabelImage() {
-        Icon icon = new ImageIcon("image/splash.png");
-        JLabel label = new JLabel("", icon, SwingConstants.CENTER);
-        return label;
+        window.dispose();
     }
 }
